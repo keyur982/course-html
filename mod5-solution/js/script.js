f7567599df9959de2867e11159995f6ed9a2d73b
Keyur Patel <keyur823827@gmail.com>
2020-07-18 18:33:29 +0530

    mod-5

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var finalHtml = homeHtml;
-      finalHtml = insertProperty(finalHtml,"randomCategoryShortName","SP");
+      finalHtml = insertProperty(finalHtml,"randomCategoryShortName",chosenCategoryShortName);
       var homeHtmlToInsertIntoMainPage = finalHtml;
 
 
