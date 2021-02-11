c24cc8e18b949663eed71f3afe0d02b086d24da4
Keyur Patel <65339527+keyur982@users.noreply.github.com>
2021-02-11 12:08:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/user.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cantarell&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+    <title>E-HealthCare</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark" style="width: 100%;position: fixed;z-index: 100;">
+        <a class="navbar-brand" style="font-size: 30px;font-family: 'Satisfy', cursive;">E-HealthCare</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+
+
+            </ul>
+            <span class="navbar-text">
+                <a href="loginui.php"><button type="button" class="btn btn-primary" style="font-size: 20px;padding-left: 20px;padding-right: 20px;">SIGN IN</button></a>
+            </span>
+        </div>
+    </nav>
+    <br><br><br><br>
+    <p style="text-align: center;font-size: 20px;">Sign In to explore more features</p>
+   
+    <br>
+    <div class="container-sm">
+        <div class="heading">
+            E-HealthCare Is Here For You
+        </div>
+        <br>
+        <div class="sub-heading">
+            Your Ultimate Health Care Provider
+        </div>
+        <br>
+        <div class="row justify-content-center">
+            <div class="col-md-7 ">
+                <div style="text-align: justify;font-size: 20px;">
+                    HealthCare offers patients exceptional care and treatment options in order to get you healthy again. We welcome you to our site and invite you to browse through to find out more about various facilities and the information we are able to provide.
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <div class="container-sm">
+        <div class="heading">
+            Wide Range of Services
+        </div>
+        <br>
+        <div class="sub-heading">
+            Personalised Care
+        </div>
+    </div>
+    <br><br><br>
+    <div class="container">
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/appointment.jpg" width="100%" height="100%" alt="Online Appointment">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        Online Appointment
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Take your appointment as early as possible to get the fast and better treatment from well-knowed doctors. Take appointment to the nearest hospital to get treated.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/hospital.jpg" width="100%" height="100%" alt="Hospitals">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        Hospitals
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Go through various hospitals to look at your best needs. Find the nearest hospital easily.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/doctor.jpg" width="100%" height="100%" alt="Doctors">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        Doctors
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Search doctor according to your requirements and availability of doctor in specific hospital at required time.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/medical.jpg" width="100%" height="100%" alt="MedicalStores">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        MedicalStores
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Get the location of the nearest medical store and check whether your required medicine is available or not without wasting your precious time.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/bloodbank.jpg" width="100%" height="100%" alt="Blood Banks">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        BloodBanks
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Never loose your beloved ones due to lack of blood. Here's the best service you need at anytime anywhere. As per your situation you can find blood in your neighbourhood.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/laboratory.jpg" width="100%" height="100%" alt="Laboratories">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        Laboratories
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Diagnose yourself as per your need nearby your place.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="base-card">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="image/surgical.jpg" width="100%" height="100%" alt="Surgical Stores">
+                </div>
+                <div class="col-md-6 ">
+                    <p class="sub-heading"><br>
+                        SurgicalStores
+                    </p>
+                    <p style="text-align: justify;padding-right: 20px; padding-left: 20px;">
+                        Find best medical instruments from the surgical store besides you.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <br><br><br><br>
+
+
+    <!-- Footer -->
+    <div class="card-footer text-muted bg-dark">
+        <div class="text-center">
+            <p style="letter-spacing: 3px; ">&copy; Copyright E-HealthCare 2020 by BUGSQUASHERS</p>
+        </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
